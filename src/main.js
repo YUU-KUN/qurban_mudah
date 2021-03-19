cece76653580b5a99922a761f1a4dae94e7798e1
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-03-19 14:14:29 +0700

    add packages to filter number format

--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueCurrency from 'vue-currency-filter'
 
 Vue.use(VueAxios, axios)
+
+Vue.use(VueCurrency, {
+  name: 'rupiah',
+  symbol : 'Rp',
+  thousandsSeparator: '.',
+  fractionCount: 2,
+  fractionSeparator: ',',
+  symbolPosition: 'front',
+  symbolSpacing: true,
+  avoidEmptyDecimals: undefined,
+})
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api'

24f55f99a3cd934069cb0d729a0cb5fc7397d89a
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-03-19 14:13:23 +0700

    add routing path

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,19 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Petugas from '../views/Petugas.vue'
+import Siswa from '../views/Siswa.vue'
+import Kelas from '../views/Kelas.vue'
+import SPP from '../views/SPP.vue'
+import Pembayaran from '../views/Pembayaran.vue'
+import HistoryPembayaran from '../views/HistoryPembayaran.vue'
 
 import Sidebar from '../views/layouts/Sidebar.vue'
 import Header from '../views/layouts/Header.vue'
 import Footer from '../views/layouts/Footer.vue'
 // import Slide from '../views/layouts/Slide.vue'
 import TopNav from '../views/layouts/TopNav.vue'
+
+import Login from '../views/auth/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +24,36 @@ const routes = [
     path: '/',
     name: 'Dashboard',
     components: {default: Dashboard, sidebar: Sidebar, header: Header, footer: Footer, topnav: TopNav}
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    components: {login: Login}
+  },
+  {
+    path: '/historyPembayaran',
+    name: 'History Pembayaran',
+    components: {default: HistoryPembayaran, sidebar: Sidebar, header: Header, footer: Footer, topnav: TopNav}
+  },
+  {
+    path: '/pembayaran',
+    name: 'Pembayaran',
+    components: {default: Pembayaran, sidebar: Sidebar, header: Header, topnav: TopNav}
+  },
+  {
+    path: '/spp',
+    name: 'SPP',
+    components: {default: SPP, sidebar: Sidebar, header: Header, footer: Footer, topnav: TopNav}
+  },
+  {
+    path: '/kelas',
+    name: 'Kelas',
+    components: {default: Kelas, sidebar: Sidebar, header: Header, footer: Footer, topnav: TopNav}
+  },
+  {
+    path: '/siswa',
+    name: 'Siswa',
+    components: {default: Siswa, sidebar: Sidebar, header: Header, footer: Footer, topnav: TopNav}
   },
   {
     path: '/petugas',

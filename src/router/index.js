3acf28300f83ee5f759c1194742de9afa3023da0
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-06-05 16:59:08 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,10 @@ import store from '../store'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Dashboard from '../views/Dashboard.vue'
-import Petugas from '../views/Petugas.vue'
-import Siswa from '../views/Siswa.vue'
-import Kelas from '../views/Kelas.vue'
 import SPP from '../views/SPP.vue'
-import Pembayaran from '../views/Pembayaran.vue'
-import HistoryPembayaran from '../views/HistoryPembayaran.vue'
 
 import Sidebar from '../views/layouts/Sidebar.vue'
 import Header from '../views/layouts/Header.vue'
-// import Slide from '../views/layouts/Slide.vue'
 import TopNav from '../views/layouts/TopNav.vue'
 
 import Register from '../views/auth/Register.vue'
@@ -40,49 +34,9 @@ const routes = [
     components: {auth: Login}
   },
   {
-    path: '/historyPembayaran',
-    name: 'History Pembayaran',
-    components: {default: HistoryPembayaran, sidebar: Sidebar, header: Header, topnav: TopNav},
-    meta: {
-      requiresAuthSiswa: true
-    }
-  },
-  {
-    path: '/pembayaran',
-    name: 'Pembayaran',
-    components: {default: Pembayaran, sidebar: Sidebar, header: Header, topnav: TopNav},
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
     path: '/spp',
     name: 'SPP',
     components: {default: SPP, sidebar: Sidebar, header: Header, topnav: TopNav},
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/kelas',
-    name: 'Kelas',
-    components: {default: Kelas, sidebar: Sidebar, header: Header, topnav: TopNav},
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/siswa',
-    name: 'Siswa',
-    components: {default: Siswa, sidebar: Sidebar, header: Header, topnav: TopNav},
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: '/petugas',
-    name: 'Petugas',
-    components: {default: Petugas, sidebar: Sidebar, header: Header, topnav: TopNav},
     meta: {
       requiresAuth: true
     }
